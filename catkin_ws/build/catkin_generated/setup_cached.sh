19fea1703ea4b559f721b5f41607bfb8156b9c2d
thehamop1 <cristian.gutierrez.56@my.csun.edu>
2019-02-15 18:32:20 -0800

    edited the directory in the makefile

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,5 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/nvidia/Desktop/formulaEmbedded/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/nvidia/Desktop/formulaEmbedded/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/nvidia/Desktop/formulaEmbedded/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/nvidia/Desktop/formulaEmbedded/catkin_ws/build"
-export ROS_PACKAGE_PATH="/home/nvidia/Github/jetsonTx2/catkin_ws/src:/home/nvidia/Desktop/jetsonTx2/catkin_ws/src:/home/nvidia/Desktop/formulaEmbedded/catkin_ws/src:/opt/ros/kinetic/share"
+export PYTHONPATH="/home/nvidia/Desktop/formulaEmbedded/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nvidia/Desktop/formulaEmbedded/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/nvidia/Github/jetsonTx2/catkin_ws/src:/home/nvidia/Desktop/jetsonTx2/catkin_ws/src:/home/nvidia/Desktop/formulaEmbedded/catkin_ws/src:$ROS_PACKAGE_PATH"
---
+++ catkin_ws/build/fsae_electric_vehicle/CMakeFiles/WHEEL_SPEED_DEVICE.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/WHEEL_SPEED_DEVICE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
