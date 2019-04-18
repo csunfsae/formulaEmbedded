f117f91ae919af48106078b196f4da9cc0e4448f
Alejandro Martinez <hello@alejandrom.xyz>
2019-04-17 18:57:14 -0700

    change

--- catkin_ws/src/fsae_electric_vehicle/src/nodejs/localServer.js
+++ catkin_ws/src/fsae_electric_vehicle/src/nodejs/localServer.js
@@ -23,15 +23,15 @@ function app() {
     .then((rosNode) => {
       rosSubscribe(rosNode, ioMessage);
       io.on('connection', function (client) {
-        client.on('accelerate', function (data) {
-          const pub = rosNode.advertise('can_bus', std_msgs.can_message);
-          const msg = new std_msgs.can_message();
-          msg.id = '201';
-          msg.data = `31${data.value}`;
-          setInterval(() => {
-            pub.publish(msg);
-          }, 1000);
-        });
+        // client.on('accelerate', function (data) {
+        //   const pub = rosNode.advertise('can_bus', std_msgs.can_message);
+        //   const msg = new std_msgs.can_message();
+        //   msg.id = '201';
+        //   msg.data = `31${data.value}`;
+        //   setInterval(() => {
+        //     pub.publish(msg);
+        //   }, 1000);
+        // });
       });
     });
 }
