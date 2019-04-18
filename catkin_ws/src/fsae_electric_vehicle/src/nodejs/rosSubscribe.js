abda47e342cdab23e7a7dc8b11486dc63ce6206e
Alejandro Martinez <hello@alejandrom.xyz>
2019-04-17 18:11:38 -0700

    Detach Nodes

--- catkin_ws/src/fsae_electric_vehicle/src/nodejs/rosSubscribe.js
+++ catkin_ws/src/fsae_electric_vehicle/src/nodejs/rosSubscribe.js
@@ -1,15 +1,39 @@
 function rosSubscribe(rosNode, ioMessage) {
     const std_msgs = rosnodejs.require('fsae_electric_vehicle').msg;
-    io.on('connection', function (client) {
-        client.on('accelerate', function (data) {
-            const pub = nh.advertise('can_bus', std_msgs.can_message);
-            const msg = new std_msgs.can_message();
-            msg.id = '201';
-            msg.data = `31${data.value}`;
-            setInterval(() => {
-                pub.publish(msg);
-            }, 1000);
-        });
+    let compass = rosNode.subscribe('compass', std_msgs.compass,
+        (data) => {
+            new ioMessage("compass", { x: data.x, y: data.y, z: data.z, device: "MPU-9255" });
+        }
+    );
+    let accelerometer = rosNode.subscribe('accelerometer', std_msgs.accelerometer,
+        (data) => {
+            new ioMessage("accelerometer", { x: data.x, y: data.y, z: data.z, device: "MPU-9255" });
+        }
+    );
+    let gyroscope = rosNode.subscribe('gyroscope', std_msgs.gyroscope,
+        (data) => {
+            new ioMessage("gyroscope", { x: data.x, y: data.y, z: data.z, device: "MPU-9255" });
+        }
+    );
+    let temp_and_pressure = rosNode.subscribe('temp_and_pressure', std_msgs.temp_and_pressure,
+        (data) => {
+            new ioMessage("temperature", { value: data.temp, device: "BMP280" });
+            new ioMessage("pressure", { value: data.pressure, device: "BMP280" });
+        }
+    );
+    let gps = rosNode.subscribe('GPS', std_msgs.gps, (data) => {
+        new ioMessage("location", { lat: data.latitude, long: data.longitude, sats: data.sats, alt: data.alt });
+    });
+
+    let steering_wheel = rosNode.subscribe('steering_wheel', std_msgs.analog_sensor, (data) => {
+        console.log(data);
+    });
+
+    let accelerator = rosNode.subscribe('accelerator', std_msgs.analog_sensor, (data) => {
+        console.log(data);
+    });
+    let can_data = rosNode.subscribe('can_bus', std_msgs.can_message, (data) => {
+        new ioMessage("canbus", {id: data.id, data:data.data, device:"can_bus"});
     });
 }
 
