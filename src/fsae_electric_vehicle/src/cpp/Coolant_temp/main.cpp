3aa43f0914cfb63f5d4aacd8361b53779bb9120e
RoySolKo <koroy123@gmail.com>
2019-11-25 10:29:32 -0800

    made progress changing to coolant stuff

--- src/fsae_electric_vehicle/src/cpp/Coolant_temp/main.cpp
+++ src/fsae_electric_vehicle/src/cpp/Coolant_temp/main.cpp
@@ -1,26 +1,28 @@
 #include "ros/ros.h"
-#include "fsae_electric_vehicle/gps.h"
+#include <fsae_electric_vehicle/coolant.h>
 
 int main(int argc, char **argv) {
-  std::cout << "WHAT!?" << std::endl;
-  ros::init(argc, argv, "GPS");
-  std::cout << "After init" << std::endl;
+  std::cout << "Starting Coolant" << std::endl;
+After init
+  ros::init(argc, argv, "Coolant");
+  std::cout << "initialized coolant node" << std::endl;
+  
   ros::NodeHandle n;
-  std::cout << "After node handle" << std::endl;
+  std::cout << "After node handle calling ros::start()" << std::endl;
 
-  ros::Publisher gps_topic = n.advertise<fsae_electric_vehicle::gps>("gps", 1000);
+  ros::Publisher coolant_msg = n.advertise<fsae_electric_vehicle::tempurature>("tempurature", 1000);
 
   std::cout << "After publisher" << std::endl;
 
-  fsae_electric_vehicle::gps gps;
+  fsae_electric_vehicle::temperature temperature;
 
-  ros::Rate loop_rate{5};
+  ros::Rate loop_rate(10);
   std::cout << "Init!" << std::endl;
 
   int x = 0;
-
+//change all to subscriber node not publisher
   while (ros::ok()) {
-    gps.latitude = x;
+    coolant.temp = x;
     x++;
     gps_topic.publish(gps);
     ros::spinOnce();
