00b47d01662d81fe5da73861fdbd0fe83cacdc8c
RoySolKo <46010217+RoySolKo@users.noreply.github.com>
2019-12-07 19:55:20 -0800

    Roy coolant (#70)
    * starting a branch
    
    * testing out git functions
    
    * made progress changing to coolant stuff
    
    * semi-finished the published the node waiting on thad for review and criticism and ass whooping
    
    * changed simulation and data type
    
    * finalized coolant

---
+++ src/fsae_electric_vehicle/src/cpp/Coolant_temp/main.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include <fsae_electric_vehicle/coolant.h>
+
+int main(int argc, char **argv) {
+  std::cout << "Starting Coolant" << std::endl;
+
+  ros::init(argc, argv, "Coolant");
+  std::cout << "initialized coolant node" << std::endl;
+  
+  ros::NodeHandle n;
+  std::cout << "After node handle calling ros::start()" << std::endl;
+
+  ros::Publisher coolant_msg = n.advertise<fsae_electric_vehicle::temperature>("temperature", 1000);
+
+  std::cout << "After publisher" << std::endl;
+  
+  fsae_electric_vehicle::temperature temperature;
+
+  ros::Rate loop_rate(5);
+  std::cout << "listening" << std::endl;
+
+  float x = 0;
+
+  while (ros::ok()) {
+    coolant.temp = x;
+    x+=static_cast <float> (rand()) / static_cast <float> (RAND_MAX); //simulation
+    coolant_msg.publish(temperature);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
